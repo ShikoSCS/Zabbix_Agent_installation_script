52de7b3330e844b22a6d4bff10d17bfa0fbe470e
Ernesto La Rosa Sanfiel <31458085+ShikoSCS@users.noreply.github.com>
2024-05-17 13:51:02 -0300

    Update and rename zab.sh to zabixx_agent_ubuntu.sh

--- zabixx_agent_ubuntu.sh
+++ zabixx_agent_ubuntu.sh
@@ -11,8 +11,8 @@ apt update &&
 apt install -y zabbix-agent &&
 
 # Configure the Zabbix agent
-sed -i 's/^\s*Server=127.0.0.1/Server=192.168.10.210,192.168.10.126/' /etc/zabbix/zabbix_agentd.conf &&
-sed -i 's/^\s*ServerActive=127.0.0.1/ServerActive=192.168.10.210,192.168.10.126/' /etc/zabbix/zabbix_agentd.conf &&
+sed -i 's/^\s*Server=127.0.0.1/Server=SERVERIP/' /etc/zabbix/zabbix_agentd.conf &&
+sed -i 's/^\s*ServerActive=127.0.0.1/ServerActive=SERVERIP/' /etc/zabbix/zabbix_agentd.conf &&
 
 # Start the Zabbix agent and enable it to start at system boot
 systemctl restart zabbix-agent &&
